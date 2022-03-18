c69d44f202d80d12395081e055238c6b4d8e220c
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-03-18 21:55:37 +0400

    Task 5 hazır

--- JsFirstSteps/js/task-5.js
+++ JsFirstSteps/js/task-5.js
@@ -9,13 +9,3 @@ let z = x - a;
 z = z.toString();
 console.log(z.slice(2));
 
-// console.log("4.234 ədədinin tam hissəsi- " + x.toFixed());
-// let y = x - x.toFixed();
-// //y = y.toFixed();
-// console.log(y);
-// y.toString();
-// console.log(y);
-// console.log(typeof y);
-// let z = y.length - 2;
-// console.log(z);
-// //console.log("4.234 ədədinin kəsrli hissəsi- " + y);
