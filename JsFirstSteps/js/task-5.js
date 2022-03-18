6f300711c63485ccb69531c41cc711d30ee9e426
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-03-18 21:48:20 +0400

    Task 5 hazır

--- JsFirstSteps/js/task-5.js
+++ JsFirstSteps/js/task-5.js
@@ -1,7 +1,21 @@
 //verilən kəsirli ədədin (nümumə: 4.234) tam hissəsi ilə
 //kəsirli hissəsini ayrı ayrı ekrana çap edən program yazın
-let x = 4.234;
-console.log("4.234 ədədinin tam hissəsi- " + x.toFixed());
-let y = x - x.toFixed();
-y = y * 1000;
-console.log("4.234 ədədinin kəsrli hissəsi- " + y);
+let x = prompt();
+x = Number(x);
+let a;
+a = x.toFixed();
+console.log(a);
+let z = x - a;
+z = z.toString();
+console.log(z.slice(2));
+
+// console.log("4.234 ədədinin tam hissəsi- " + x.toFixed());
+// let y = x - x.toFixed();
+// //y = y.toFixed();
+// console.log(y);
+// y.toString();
+// console.log(y);
+// console.log(typeof y);
+// let z = y.length - 2;
+// console.log(z);
+// //console.log("4.234 ədədinin kəsrli hissəsi- " + y);
