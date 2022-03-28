e4ede7481bab0df6f314ad972bd1a186c2d96486
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-03-28 20:52:53 +0400

    Yeni tapşırığın fayl strukturu yaradıldı

--- JsFirstSteps/js/task-5.js
+++ JsFirstSteps/js/task-5.js
@@ -1,11 +1,6 @@
 //verilən kəsirli ədədin (nümumə: 4.234) tam hissəsi ilə
 //kəsirli hissəsini ayrı ayrı ekrana çap edən program yazın
-let x = prompt();
-x = Number(x);
-let a;
-a = x.toFixed();
-console.log(a);
-let z = x - a;
-z = z.toString();
-console.log(z.slice(2));
-
+let eded = 4.234;
+let tam = eded.toFixed();
+console.log(tam);
+console.log((eded - tam) * 1000);
