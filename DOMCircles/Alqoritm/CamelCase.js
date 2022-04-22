f00c56686b2d58fef2d22b99372046e9816f2e8e
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-04-22 20:34:08 +0400

    Task Done

---
+++ DOMCircles/Alqoritm/CamelCase.js
@@ -0,0 +1,13 @@
+"use strict";
+let a = "saveChangesInTheEditor";
+function camelcase(s) {
+  let say = 1;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == s[i].toUpperCase()) {
+      say++;
+    }
+  }
+  console.log(say);
+  return say;
+}
+camelcase(a);
