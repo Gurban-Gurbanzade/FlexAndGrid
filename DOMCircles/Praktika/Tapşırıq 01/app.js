b556330e5f184748faa2c38c308a00a0cbb7cf7b
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-04-22 19:35:43 +0400

    Tapşırıq 01 Hazır

--- DOMCircles/Praktika/Tapşırıq 01/app.js
+++ DOMCircles/Praktika/Tapşırıq 01/app.js
@@ -1,15 +1,15 @@
 function createCircle(_selector, _radius, _backgroundColor) {
+  let elementİnHtml = document.createElement("div");
   let daire = document.createElement("div");
+  elementİnHtml.className = `${_selector}`;
   let radius = _radius * 2;
   daire.style.width = `${radius}px`;
   daire.style.height = `${radius}px`;
-  daire.style.backgroundColor = `${_backgroundColor}`;
   daire.style.borderRadius = "50%";
-  document.querySelector(`${_selector}`).appendChild(daire);
+  daire.style.backgroundColor = `${_backgroundColor}`;
+  document.querySelector("html").appendChild(elementİnHtml).appendChild(daire);
 }
 
 createCircle(".box", 100, "red");
 
-// ekranda 2 box verməyimin səbəbi şərtdə deyildiyi kimi hansı html elementi daxil edilsə dairə onun daxilində yaranacaq
 // radiusu 2-ə vurmağımın səbəbi adi riyazi qaydadır, diametri tapmaq üçündür.
-//
