c7613ac85de635a3117e9495a405757ff899aee1
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-04-30 03:59:39 +0400

    Tapşırıq 10 Hazır

--- TagSelector&Commenting/BackgroundSwitcher/app.js
+++ TagSelector&Commenting/BackgroundSwitcher/app.js
@@ -3,10 +3,36 @@ function onOff() {
   if (x.style.backgroundColor == "rgb(37, 46, 72)") {
     x.style.backgroundColor = "rgb(238, 245, 255)";
     document.querySelector(".light").style.color = "#262f49";
-    document.querySelector(".dark").style.color = "262f49";
+    document.querySelector(".dark").style.color = "gray";
+    document.querySelector(".controller").style.backgroundColor = "#82ADFF";
+    document.querySelector(".controller").children[0].className =
+      "fa-solid fa-circle noqte4";
+    document.querySelector(".controller").children[1].className =
+      "fa-solid fa-circle fa-1x noqte5";
+    document.querySelector(".controller").children[2].className =
+      "fa-solid fa-circle fa-3x noqte6";
+    document.querySelector(".controller").children[3].className =
+      "fa-solid fa-circle noqte4";
+    document.querySelector(".controller").children[4].className =
+      "fa-solid fa-circle fa-1x noqte5";
+    document.querySelector(".controller").children[5].className =
+      "fa-solid fa-circle fa-3x noqte6";
   } else {
     x.style.backgroundColor = "#252E48";
     document.querySelector(".light").style.color = "#40485F";
     document.querySelector(".dark").style.color = "white";
+    document.querySelector(".controller").style.backgroundColor = "#0C1019";
+    document.querySelector(".controller").children[0].className =
+      "fa-solid fa-star fa-xs ulduzSmall";
+    document.querySelector(".controller").children[1].className =
+      "fa-solid fa-star ulduzBig";
+    document.querySelector(".controller").children[2].className =
+      "fa-solid fa-moon fa-4x ay fa-flip-horizontal";
+    document.querySelector(".controller").children[3].className =
+      "fa-solid fa-circle  noqte1";
+    document.querySelector(".controller").children[4].className =
+      "fa-solid fa-circle noqte2";
+    document.querySelector(".controller").children[5].className =
+      "fa-solid fa-circle noqte3";
   }
 }
