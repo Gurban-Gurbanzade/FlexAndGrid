6171e6e01d28e591d4c9176ca169a87c6612204c
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-04-30 00:39:23 +0400

    Tapşırıq 12 Hazır

---
+++ TagSelector&Commenting/CommentingUIAddComment/app.js
@@ -0,0 +1,27 @@
+function select() {
+  let x = document.querySelector("input");
+  let z = Number(document.querySelector(".commentsCountNumber").innerText);
+
+  if (x == "") {
+    alert("Zəhmət olmasa şərh bildirin.");
+  } else {
+    let a = ` <div class="comment">
+  <div class="pic">
+    <img class="commentImg" src="Image/girl.jpg" alt="" />
+  </div>
+  <div class="commentInfo">
+    <h1>Michael Sena</h1>
+    <p class="commentDate">8 months ago</p>
+    <p class="commentId">0x2a0D29...269BBb6</p>
+    <p class="commentText" style="font-size: large">
+      ${x.value}
+    </p>
+  </div>
+</div> `;
+    document.querySelector("main").insertAdjacentHTML("afterbegin", a);
+    x.value = "";
+    z++;
+    document.querySelector(".commentsCountNumber").innerText = `${z}`;
+  }
+}
+console.log(document.querySelector(".commentsCountNumber").innerText);
