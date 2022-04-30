51bd844db26c4e9ceae406288654f1616223f7c2
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-04-30 04:47:24 +0400

    Tapşırıq 08 Hazırdır

--- TagSelector&Commenting/TagSelectorSingleFull/app.js
+++ TagSelector&Commenting/TagSelectorSingleFull/app.js
@@ -1,6 +1,9 @@
 "use strict";
 function select(e) {
+  let x = document.querySelector(".page-container-movies-name");
   let z = `<i class="fa-regular fa-circle-check"></i>`;
+  let arr = [];
+  arr = x;
   if (e.target.children.length == 0) {
     e.target.insertAdjacentHTML("beforeend", z);
     e.target.setAttribute("class", "activeName");
@@ -8,13 +11,19 @@ function select(e) {
     e.target.children[0].remove();
     e.target.classList.remove("activeName");
   }
+  for (let i = 0; i < x.children.length; i++) {
+    if (e.target !== x.children[i] && x.children[i].children.length !== 0) {
+      x.children[i].children[0].remove();
+      x.children[i].classList.remove("activeName");
+    }
+  }
 }
 
 function createFilm() {
   let x;
   if (event.keyCode == 13) {
     x = document.querySelector("input");
-    let a = `<p onclick="select(event)" class="activeName">${x.value}<i class="fa-regular fa-circle-check"></i></p>`;
+    let a = `<p onclick="select(event)">${x.value}</p>`;
     if (x.value !== "") {
       document
         .querySelector(".page-container-movies-name")
