cee3e02d8bc95dd045e6d4d59b2bc342bddacaee
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-05-05 19:45:43 +0400

    fayl strukturu yaradıldı

---
+++ 01/app.js
@@ -0,0 +1,15 @@
+// class priceList extends HTMLElement {
+//   constructor() {
+//     super();
+//   }
+// }
+
+// customElements.define("price-list", priceList);
+// priceList = new priceList();
+// priceList.innerHTML = "Salam dunya";
+// priceList.style.color = "red";
+// priceList.className = "baku";
+// document.body.appendChild(priceList);
+
+// document.querySelector("price-list").style.color = "yellow";
+// //document.querySelector(".baku").style.color = "green";
